dbc587bc285bb6bf2ba01eba20571c85caa5106a
Rahul Nene <rahul.nene@live.com>
2023-09-10 10:39:13 +0530

    Hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+mod color;
+mod vec;
+
+use color::Color;
+use std::io;
 fn main() {
-    println!("Hello, world!");
+    // Image
+
+    const IMAGE_WIDTH: i32 = 256;
+    const IMAGE_HEIGHT: i32 = 256;
+
+    // Render
+
+    print!("P3\n{IMAGE_WIDTH} {IMAGE_HEIGHT}\n255\n");
+
+    for j in (0..IMAGE_HEIGHT).rev() {
+        for i in 0..IMAGE_WIDTH {
+            let r = i as f64 / (IMAGE_WIDTH - 1) as f64;
+            let g = j as f64 / (IMAGE_HEIGHT - 1) as f64;
+            let b = 0.25;
+
+            let pixel_color = Color::new(r, g, b);
+            color::write_color(&mut io::stdout(), pixel_color);
+        }
+    }
 }
---
+++ src/vec.rs
@@ -0,0 +1,126 @@
+use std::fmt::{Display, Formatter, Result};
+use std::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Neg, Sub, SubAssign};
+
+#[derive(Debug, Copy, Clone)]
+pub struct Vec3 {
+    coords: [f64; 3],
+}
+
+impl Vec3 {
+    pub fn new(x: f64, y: f64, z: f64) -> Self {
+        Self { coords: [x, y, z] }
+    }
+
+    pub fn x(&self) -> f64 {
+        self.coords[0]
+    }
+    pub fn y(&self) -> f64 {
+        self.coords[1]
+    }
+    pub fn z(&self) -> f64 {
+        self.coords[2]
+    }
+
+    pub fn magnitude(&self) -> f64 {
+        self.length_squared().sqrt()
+    }
+    pub fn length_squared(&self) -> f64 {
+        self.coords.iter().map(|c| c * c).sum()
+    }
+
+    pub fn dot(&self, rhs: Self) -> f64 {
+        self.coords
+            .iter()
+            .zip(rhs.coords.iter())
+            .map(|(a, b)| a * b)
+            .sum()
+    }
+
+    pub fn cross(&self, rhs: Self) -> Self {
+        Self::new(
+            self.y() * rhs.z() - self.z() * rhs.y(),
+            self.z() * rhs.x() - self.x() * rhs.z(),
+            self.x() * rhs.y() - self.y() * rhs.x(),
+        )
+    }
+}
+
+pub type Point3 = Vec3;
+
+impl Display for Vec3 {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        write!(f, "{} {} {}", self.x(), self.y(), self.z())
+    }
+}
+
+impl Neg for Vec3 {
+    type Output = Self;
+
+    fn neg(self) -> Self::Output {
+        Self::new(-self.x(), -self.y(), -self.z())
+    }
+}
+
+pub fn normalize(v: Vec3) -> Vec3 {
+    v / v.magnitude()
+}
+
+impl Add for Vec3 {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self::new(self.x() + rhs.x(), self.y() + rhs.y(), self.z() + rhs.z())
+    }
+}
+impl AddAssign for Vec3 {
+    fn add_assign(&mut self, rhs: Self) {
+        *self = *self + rhs;
+    }
+}
+
+impl Sub for Vec3 {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self::new(self.x() - rhs.x(), self.y() - rhs.y(), self.z() - rhs.z())
+    }
+}
+impl SubAssign for Vec3 {
+    fn sub_assign(&mut self, rhs: Self) {
+        *self = *self - rhs;
+    }
+}
+
+impl Mul<f64> for Vec3 {
+    type Output = Self;
+
+    fn mul(self, rhs: f64) -> Self::Output {
+        Self::new(self.x() * rhs, self.y() * rhs, self.z() * rhs)
+    }
+}
+impl MulAssign<f64> for Vec3 {
+    fn mul_assign(&mut self, rhs: f64) {
+        *self = *self * rhs;
+    }
+}
+
+impl Div<f64> for Vec3 {
+    type Output = Self;
+
+    fn div(self, rhs: f64) -> Self::Output {
+        self * (1.0 / rhs)
+    }
+}
+impl DivAssign<f64> for Vec3 {
+    fn div_assign(&mut self, rhs: f64) {
+        *self = *self / rhs;
+    }
+}
+
+impl Mul<Vec3> for f64 {
+    type Output = Vec3;
+
+    fn mul(self, v: Vec3) -> Vec3 {
+        Vec3::new(self * v.x(), self * v.y(), self * v.z())
+    }
+}
